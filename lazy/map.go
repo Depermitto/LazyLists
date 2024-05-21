e198957a2feba624b9287a4a4985ccb6a9021ee2
permito <penciller@disroot.org>
2024-05-21 19:43:35 +0200

    add map

---
+++ lazy/map.go
@@ -0,0 +1,17 @@
+package lazy
+
+func mapTo[T, V any](iter Seq[T], f func(T) V) Seq[V] {
+	return func(yield func(V) bool) {
+		for t := range iter {
+			yield(f(t))
+		}
+	}
+}
+
+func Map[T, V any](s []T, f func(T) V) Seq[V] {
+	return mapTo(makeIter(s), f)
+}
+
+func (iter Seq[T]) Map(f func(T) T) Seq[T] {
+	return mapTo(iter, f)
+}
