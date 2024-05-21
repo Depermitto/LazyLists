11bac2269a3a20cc642baad8bc936ddc35d1671c
permito <penciller@disroot.org>
2024-05-21 21:55:38 +0200

    add reverse map and reverse fold

--- lazy/reverse.go
+++ lazy/reverse.go
@@ -7,3 +7,11 @@ func Reverse[T any](s []T) Seq[T] {
 		}
 	}
 }
+
+func ReverseMap[T, V any](s []T, f func(T) V) Seq[V] {
+	return mapTo(Reverse(s), f)
+}
+
+func ReverseFold[T, V any](s []T, init V, f func(V, T) V) V {
+	return fold(Reverse(s), init, f)
+}
