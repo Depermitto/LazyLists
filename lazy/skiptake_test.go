27b3fc24f588124758535d19183fa11bc829a370
permito <penciller@disroot.org>
2024-05-23 00:18:58 +0200

    remove old tests

--- lazy/skiptake_test.go
+++ lazy/skiptake_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-const testSliceSize = 200
+const testSliceSize = 20000
 
 func TestTakeWhile(t *testing.T) {
 	s := rand.Perm(testSliceSize)
@@ -29,18 +29,6 @@ func BenchmarkTakeWhile(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		for t := range TakeWhile(s, func(i int) bool { return i != -10 }) {
-			_ = t
-		}
-	}
-}
-
-func BenchmarkTakeWhileOld(b *testing.B) {
-	s := Range(0, testSliceSize).Map(func(int) int { return 4 }).Collect()
-	s[0] = -10
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		for t := range TakeWhileOld(s, func(i int) bool { return i != -10 }) {
 			_ = t
 		}
 	}
