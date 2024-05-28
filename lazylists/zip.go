bddf10eccf22e0e85f951148143d4c6a6ec3170d
permito <penciller@disroot.org>
2024-05-28 12:10:31 +0200

    make zips into Seq2

--- lazylists/zip.go
+++ lazylists/zip.go
@@ -1,23 +1,22 @@
 package lazylists
 
 import (
-	"github.com/barweiss/go-tuple"
 	"iter"
 )
 
-func Zip[T, U any](seq1 Seq[T], seq2 Seq[U]) Seq[tuple.T2[T, U]] {
-	next1, stop1 := iter.Pull[T](iter.Seq[T](seq1))
-	next2, stop2 := iter.Pull[U](iter.Seq[U](seq2))
+func Zip[I, T any](seq1 Seq[I], seq2 Seq[T]) Seq2[I, T] {
+	next1, stop1 := iter.Pull[I](iter.Seq[I](seq1))
+	next2, stop2 := iter.Pull[T](iter.Seq[T](seq2))
 
-	return func(yield func(tuple.T2[T, U]) bool) {
+	return func(yield func(I, T) bool) {
 		for {
-			t, ok1 := next1()
-			u, ok2 := next2()
+			i, ok1 := next1()
+			t, ok2 := next2()
 			if !ok1 || !ok2 {
 				goto stop
 			}
 
-			yield(tuple.New2(t, u))
+			yield(i, t)
 		}
 	stop:
 		stop1()
